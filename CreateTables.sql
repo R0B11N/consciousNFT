84e22791eeaf7b783aaa5f6a645f88dc914d2e16
JB Sloan <james@nuarca.com>
2023-09-23 08:50:20 -0400

    Updated data model

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,72 +1,63 @@
--- Create Project table
-CREATE TABLE Project (
-ProjectID INT AUTO_INCREMENT PRIMARY KEY,
-Name TEXT NOT NULL,
-ContractAddress CHAR(42) NOT NULL
-);
-
--- Create ProjectScenarios table
-CREATE TABLE ProjectScenarios (
-ScenarioID INT AUTO_INCREMENT PRIMARY KEY,
-ProjectID INT,
-Name TEXT NOT NULL,
-Description TEXT,
-Prompt TEXT,
-BackgroundImage TEXT,
-FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)
-);
-
--- Create ProjectLore table
-CREATE TABLE ProjectLore (
-LoreID INT AUTO_INCREMENT PRIMARY KEY,
-ProjectID INT,
-DocumentName TEXT NOT NULL,
-DocumentPath TEXT,
-UploadDate DATE,
-FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID)
-);
-
 -- Create User table
+
 CREATE TABLE User (
-UserID INT AUTO_INCREMENT PRIMARY KEY,
-WalletAddress CHAR(42) NOT NULL,
-UserName TEXT,
-Email TEXT,
-SignUpDate DATE
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    WalletAddress CHAR(42) NOT NULL,
+    UserName VARCHAR(255) UNIQUE,
+    Email VARCHAR(255) UNIQUE,
+    SignUpDate DATE
+);
+	
+-- Create Project table
+
+CREATE TABLE Project (
+    ProjectID INT AUTO_INCREMENT PRIMARY KEY,
+    Name TEXT NOT NULL,
+    ContractAddress CHAR(42) NOT NULL,
+    OwnerID INT,
+    FOREIGN KEY (OwnerID) REFERENCES User(UserID)
+);
+
+
+-- Create Questions table
+CREATE TABLE Questions (
+    QuestionID INT AUTO_INCREMENT PRIMARY KEY,
+    Question TEXT,
+    QuestionType TEXT -- Indicates if it is for Characters or Projects
+);
+CREATE INDEX idx_questiontype ON Questions(QuestionType);
+
+
+
+-- Create ProjectData table
+CREATE TABLE ProjectData (
+    AnswerID INT AUTO_INCREMENT PRIMARY KEY,
+    QuestionID INT,
+    ProjectID INT,
+    Answer TEXT,
+    FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+    FOREIGN KEY (QuestionID) REFERENCES Questions(QuestionID)
 );
 
 -- Create ProjectCharacters table
 CREATE TABLE ProjectCharacters (
-CharacterID INT AUTO_INCREMENT PRIMARY KEY,
-ProjectID INT,
-UserID INT,
-CharacterName TEXT NOT NULL,
-CharacterDescription TEXT,
-FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
-FOREIGN KEY (UserID) REFERENCES User(UserID)
+    CharacterID INT AUTO_INCREMENT PRIMARY KEY,
+    TokenID INT,
+    ProjectID INT,
+    UserID INT,
+    CharacterName TEXT NOT NULL,
+    CharacterDescription TEXT,
+    FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+    FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
--- Create CharacterDialog table
-CREATE TABLE CharacterDialog (
-DialogID INT AUTO_INCREMENT PRIMARY KEY,
-CharacterID INT,
-DialogQuestion TEXT,
-DialogAnswer TEXT,
-FOREIGN KEY (CharacterID) REFERENCES ProjectCharacters(CharacterID)
+-- Create CharacterData table
+CREATE TABLE CharacterData (
+    AnswerID INT AUTO_INCREMENT PRIMARY KEY,
+    CharacterID INT,
+    QuestionID INT,
+    Answer TEXT NOT NULL,
+    FOREIGN KEY (CharacterID) REFERENCES ProjectCharacters(CharacterID)
 );
 
--- Create CharacterQuestions table
-CREATE TABLE CharacterQuestions (
-QuestionID INT AUTO_INCREMENT PRIMARY KEY,
-Question TEXT NOT NULL
-);
 
--- Create CharacterAnswers table
-CREATE TABLE CharacterAnswers (
-AnswerID INT AUTO_INCREMENT PRIMARY KEY,
-CharacterID INT,
-QuestionID INT,
-Answer TEXT NOT NULL,
-FOREIGN KEY (CharacterID) REFERENCES ProjectCharacters(CharacterID),
-FOREIGN KEY (QuestionID) REFERENCES CharacterQuestions(QuestionID)
-);

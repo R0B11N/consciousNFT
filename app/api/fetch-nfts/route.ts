f2572741a7b7a9435ad5be99720dd7fb072aa42a
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-09-23 12:05:05 -0400

    * Dynamically displaying fetched nfts.

--- app/api/fetch-nfts/route.ts
+++ app/api/fetch-nfts/route.ts
@@ -1,14 +1,19 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
+  const { address } = await request.json();
+
+  const BASE_URL = "https://api.1inch.dev/nft/v1/byaddress";
+
   return NextResponse.json(
     {
-      imageUrl: 'https://i.seadn.io/gae/WUk8oxK66zz9byLJhzWutxXGQbDeEXwsung8AbnmM9wbBD94AdJAiTvkwYht-QYc6Q19axztu2ufHOxuqftq65FO83wNufpsOjHQIg?auto=format&dpr=1&w=1000'
+      nfts: [{
+        imageUrl: 'https://i.seadn.io/gae/WUk8oxK66zz9byLJhzWutxXGQbDeEXwsung8AbnmM9wbBD94AdJAiTvkwYht-QYc6Q19axztu2ufHOxuqftq65FO83wNufpsOjHQIg?auto=format&dpr=1&w=1000'
+      }]
     },
     {
       status: 200,
     },
   );
 }
-
--- app/individual-owner/page.tsx
+++ app/individual-owner/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import {
   Card,
   CardContent,
@@ -8,18 +11,57 @@ import {
 } from "@/components/ui/card"
 
 export default function IndividualOwner() {
+  // Obtain through WalletConnect/Rainbow.
+  const address = '0x2666f0C8FB58d182f2Dd79475DCA4A07B3724607';
+
+  const [nfts, setNfts] = useState([]);
+  console.log('nfts:', nfts);
+
+  useEffect(() => {
+    const fetchNfts = async () => {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          address
+        })
+      };
+
+      const response = await fetch('http://localhost:3000/api/fetch-nfts', options);
+
+      const json = await response.json();
+
+      const fetchedNfts = json.nfts;
+
+      setNfts(fetchedNfts);
+    }
+
+    fetchNfts();
+  }, [address, setNfts]);
+
   return <>
-    <Card>
-      <CardHeader>
-        <CardTitle className='text-blue-600'>Your NFTs</CardTitle>
-        <CardDescription>Choose any to add lore and train</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <div className='container'>
+      <h2>Wallet address: {address}</h2>
+      {
+        nfts.map(({ imageUrl }, index) => {
+          return (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle className='text-blue-600'>Your NFTs</CardTitle>
+                <CardDescription>Choose any NFT to add lore and train</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <img src={imageUrl} />
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          );
+        })
+      }
+    </div>
   </>
 }

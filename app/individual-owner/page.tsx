d575b67e8184ab825f370f10ecd22f0194c220e8
Louis Wilbrink <louis.wilbrink@gmail.com>
2023-09-22 21:13:01 -0400

    * Scaffolded NextJS app with Prisma.

---
+++ app/individual-owner/page.tsx
@@ -0,0 +1,5 @@
+export default function IndividualOwner() {
+  return (
+    <h1>Individual Owner</h1>
+  );
+}
---
+++ app/project-owner/page.tsx
@@ -0,0 +1,5 @@
+export default function ProjectOwner() {
+  return (
+    <h1>Project Owner</h1>
+  );
+}

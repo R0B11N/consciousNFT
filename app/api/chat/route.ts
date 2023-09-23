82ac809ee4c979c631e8f134b28df474376e0364
Kamalesh Palanisamy <kamalesh800@gmail.com>
2023-09-23 11:41:13 -0400

    Add integration with OpenAI

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,24 +3,29 @@ import type { NextRequest } from 'next/server';
 import OpenAI from 'openai';
 
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
+    console.log("Sending POST request to ChatGPT API");
+
     const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY,
     });
 
+    const { messages } = await request.json();
+
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: "Say this is a test" }],
+        messages,
         model: "gpt-3.5-turbo",
     });
 
+    console.log(`ChatGPT reply ${chatCompletion.choices[0].message.content}`);
 
     return NextResponse.json(
     {
-        body: request.body,
-        path: request.nextUrl.pathname,
-        query: request.nextUrl.search,
         cookies: request.cookies.getAll(),
-        message: chatCompletion.choices[0].message.content
+        reply: {
+            role: "assistant",
+            content: chatCompletion.choices[0].message.content
+        }
     },
     {
         status: 200,
